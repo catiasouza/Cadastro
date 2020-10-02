4b55bd6160998ffd437e31ec1b581f210bde452d
Catia <catiamsouza80@gmail.com>
2020-10-02 08:26:00 -0300

    delegate

--- Cadastro/Views/CadastroEndereco.swift
+++ Cadastro/Views/CadastroEndereco.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+protocol CadastroEnderecoProtocol: class {
+    func successRegister(address: Endereco
+    )
+}
+
 class CadastroEndereco: UIViewController {
 
    
@@ -16,10 +21,39 @@ class CadastroEndereco: UIViewController {
     @IBOutlet weak var cidadeTextField: UITextField!
     @IBOutlet weak var bairroTextField: UITextField!
     
+    @IBOutlet weak var estadoTextField: UITextField!
+    
+    var delegate: CadastroEnderecoProtocol?
+    
+    fileprivate func configTextFields(){
+        
+    
+        self.ruaTextField.delegate = self
+        self.bairroTextField.delegate = self
+        self.cidadeTextField.delegate = self
+        self.complementoTextField.delegate = self
+        self.estadoTextField.delegate = self
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.configTextFields()
        
+    }
+    
+    @IBAction func clicouCadastrarEndereco(_ sender: UIButton) {
+        
+        if self.ruaTextField.text?.isEmpty == true || self.bairroTextField.text?.isEmpty == true || self.complementoTextField.text?.isEmpty == true || self.cidadeTextField.text?.isEmpty == true || self.estadoTextField.text?.isEmpty == true {
+            print("campo p preencher")
+            
+        }else{
+            
+            var endereco = Endereco(rua: self.ruaTextField.text, complemento: self.complementoTextField.text, bairro: self.bairroTextField.text ,cidade: self.cidadeTextField.text, estado: self.estadoTextField.text)
+            
+            //dispara delegate
+            self.delegate?.successRegister(address: endereco)
+            self.dismiss(animated: true, completion: nil)
+        }
+        
     }
     
     @IBAction func clicouVoltou(_ sender: UIButton) {
@@ -27,4 +61,20 @@ class CadastroEndereco: UIViewController {
     }
     
 
+}
+extension CadastroEndereco:UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    switch textField {
+    case self.ruaTextField:
+        self.complementoTextField.becomeFirstResponder()
+    case self.complementoTextField:
+        self.cidadeTextField.becomeFirstResponder()
+    case self.cidadeTextField:
+        self.estadoTextField.becomeFirstResponder()
+    default:
+        self.estadoTextField.resignFirstResponder()
+    }
+    return true
+}
 }
--- Cadastro/Views/FirstVC.swift
+++ Cadastro/Views/FirstVC.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class FirstVC: UIViewController {
     
+    
+    @IBOutlet weak var estadoTextField: UITextField!
+    
     @IBOutlet weak var ruaTextField: UITextField!
     @IBOutlet weak var complementoTextField: UITextField!
     @IBOutlet weak var cidadeTextField: UITextField!
@@ -25,6 +28,9 @@ class FirstVC: UIViewController {
     
     @IBOutlet weak var enderecoButton: UIButton!
     
+    var isRegister:Bool = false
+    var usuario:Usuario?
+    
     fileprivate func configScreen(){
         
         self.enderecoView.isHidden = true
@@ -38,21 +44,42 @@ class FirstVC: UIViewController {
         self.emailTextField.delegate = self
         self.dataNascimentoTextField.delegate = self
         
-//        self.ruaTextField.delegate = self
-//        self.bairroTextField.delegate = self
-//        self.cidadeTextField.delegate = self
-//        self.complementoTextField.delegate = self
+        //        self.ruaTextField.delegate = self
+        //        self.bairroTextField.delegate = self
+        //        self.cidadeTextField.delegate = self
+        //        self.complementoTextField.delegate = self
         
         self.ruaTextField.isUserInteractionEnabled = false
         self.bairroTextField.isUserInteractionEnabled = false
         self.cidadeTextField.isUserInteractionEnabled = false
         self.complementoTextField.isUserInteractionEnabled = false
+        self.estadoTextField.isUserInteractionEnabled = false
     }
+    
+    fileprivate func validadeFields(textField: UITextField) {
+        
+        if let value = textField.text {
+            
+            if value.isEmpty {
+                self.isRegister = false
+            }else{
+                self.isRegister = true
+            }
+            
+        }else {
+            self.isRegister = false
+        }
+        
+        print("=======validateFields=====  \(self.isRegister)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       configScreen()
+        configScreen()
         configTextFields()
+        
+        
         
         
     }
@@ -68,22 +95,58 @@ class FirstVC: UIViewController {
     
     
     @IBAction func cadastrarEndereco(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "CadastroEnderecoVC", sender: nil)
+        
+        if self.nomeTextFiled.text?.isEmpty == true || self.cpfTextField.text?.isEmpty == true || self.emailTextField.text?.isEmpty == true || self.dataNascimentoTextField.text?.isEmpty == true{
+            print("campo p preencher")
+            
+        }else{
+            self.usuario = Usuario(nome: self.nomeTextFiled.text, cpf: self.cpfTextField.text, email: self.emailTextField.text, dn: self.dataNascimentoTextField.text, endereco: nil)
+            self.performSegue(withIdentifier: "CadastroEnderecoVC", sender: nil)
+        }
+        
     }
+    
+    
+    
     
     @IBAction func tappedCadastrarButton(_ sender: UIButton) {
         
         self.performSegue(withIdentifier: "DetailVC", sender: nil)
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        let vc: DetailsVC? = segue.destination as? DetailsVC
-        vc?.view.backgroundColor = .darkGray
-        vc?.myLabel.text = "Catia"
+        if segue.identifier == "CadastroEnderecoVC"{
+            
+            let vc:  CadastroEndereco? = segue.destination as? CadastroEndereco
+            vc?.delegate = self
+        }else{
+            
+            let vc: DetailsVC? = segue.destination as? DetailsVC
+            vc?.view.backgroundColor = .darkGray
+            //vc?.myLabel.text = "Catia"
+        }
+
     }
 }
+extension FirstVC: CadastroEnderecoProtocol{
+    func successRegister(address: Endereco) {
+        self.enderecoView.isHidden = false
+        self.usuario?.endereco = address
+        
+        self.ruaTextField.text = self.usuario?.endereco?.rua
+        self.complementoTextField.text = self.usuario?.endereco?.complemento
+        self.cidadeTextField.text = self.usuario?.endereco?.cidade
+        self.bairroTextField.text = self.usuario?.endereco?.bairro
+        self.estadoTextField.text = self.usuario?.endereco?.estado
+        
+        
+    }
+}
+
 extension FirstVC: UITextFieldDelegate{
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.validadeFields(textField: textField)
         switch textField {
         case self.nomeTextFiled:
             self.cpfTextField.becomeFirstResponder()
@@ -95,5 +158,9 @@ extension FirstVC: UITextFieldDelegate{
             self.dataNascimentoTextField.becomeFirstResponder()
         }
         return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.validadeFields(textField: textField)
     }
 }
